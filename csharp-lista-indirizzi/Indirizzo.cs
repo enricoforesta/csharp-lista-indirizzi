654f375c475ceedb2e3640017e73823344cf5313
enricoforesta <enrico.foresta97@gmail.com>
2024-04-29 16:39:49 +0200

    feat: stampa a schermo errori.

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        private string street;
+        public string Street
+            {
+                get { return street; }
+                set
+                {
+                    if (value.Length < 10)
+                    {
+                        throw new ArgumentException("Lo street deve avere almeno 10 caratteri.");
+                    }
+                    street = value;
+                }
+            }
+        public string City { get; set; }
+
+        private string province;
+        public string Province { get { return province; } set 
+            {
+                if(value.Length != 2)
+                {
+                    throw new ArgumentException("La provincia deve essere di 2 caratteri");
+                }
+                province = value;
+            } 
+        }
+        public int ZIP { get; set; }
+   
+
+        public Indirizzo(string name, string surname, string street, string city, string province, int zip) 
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZIP = zip;
+        }
+        public override string ToString()
+        {
+            return $"{this.Name}, {this.Surname},{this.Street},{this.City},{this.Province},{this.ZIP}";
+        }
+    }
+}
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -2,9 +2,50 @@
 {
     internal class Program
     {
+        static List<Indirizzo> indirizzo = new List<Indirizzo>();
+        static string path = "C:\\Users\\User\\Desktop\\ENRICO\\1 GITHUB\\C#\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\addresses.csv";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GetFile(path);
+            foreach(var item in indirizzo)
+            {
+              
+                Console.WriteLine(item.ToString());
+                Console.WriteLine();
+            }
+           
+        }
+
+        public static List<Indirizzo> GetFile(string path)
+        {
+            var stream = File.OpenText(path);
+            int i = 0;
+            while(stream.EndOfStream == false)
+            {
+                var linea = stream.ReadLine();
+                i++;
+                if(i <= 1) continue;
+                try 
+                {
+                    var result = linea.Split(",");
+                    string name = result[0];
+                    string surname = result[1];
+                    string street = result[2];
+                    string city = result[3];
+                    string province = result[4];
+                    int zip = int.Parse(result[5]);
+
+                    Indirizzo Indirizzo1 = new Indirizzo(name, surname, street, city, province, zip);
+                    indirizzo.Add(Indirizzo1);
+                }
+                catch (Exception e) 
+                {
+                    Console.WriteLine($"Errore alla riga {i}: {e.Message}");
+                }
+            }
+            stream.Close();
+            return indirizzo;
         }
     }
 }
